5fd02abc2cf0a51c89da9fc4510a86a2a8e15119
sonalRakholiya <sonalpipaliya1993@gmail.com>
2021-07-09 18:45:02 +0530

    Add Contact Page

--- src/Componets/AddContacts.js
+++ src/Componets/AddContacts.js
@@ -1,14 +1,32 @@
-import { Grid } from "@material-ui/core";
+import { } from "react-bootstrap";
 
- const AddContacts = () => {
+
+const AddContacts = () => {
     return (
         <div className="pageBG">
-          <div>
-          <Grid className="loginContainer" justifyContent="center" container>
-
-              </Grid>
-          </div>
-
+            <div className="loginContainer">
+                <div className=" login">
+                    <form>
+                    <div className='col-sm-12  p-2   text-center'>ADD CONTACTS</div>
+                        <div className='col-sm-12  p-2'>
+                            <div className='from-group '>
+                                <input type='text' className="form-control " placeholder='Name' />
+                            </div>
+                        </div>
+                        <div className='col-sm-12 mb-2 p-2'>
+                            <div className='from-group '>
+                                <input type='text' className="form-control " placeholder='Email' />
+                            </div>
+                        </div>
+                        <div className='col-md-12 p-2'>
+                        <div className='from-group btnGrp'>
+                            <input type='submit' className="form-control primary btnLogin"  value="ADD"  ></input>
+                        </div>
+                    </div>
+                    </form>
+                    
+                </div>
+            </div>
         </div>
     )
 }
